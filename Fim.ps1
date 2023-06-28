fb205cc2f1b9d6761a6907ce59cc497c80e0478f
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-27 23:14:13 -0400

    hash for txt spilts.

--- Fim.ps1
+++ Fim.ps1
@@ -8,9 +8,14 @@ $response = Read-Host -Prompt "Please enter 'A' or 'B' "
 
  if ($response -eq "A".toUpper()) {
     # Calculate hash from the target files and store in baseline.txt
-    Write-Host "Calculate hashes, make new baseline.txt"
+    Write-Host "Calculate hashes, make new baseline.txt" -ForegroundColor Cyan
  }
  elseif ($response -eq "B".ToUpper()) {
     # Begin monitoring files with saved baseline
-    Write-Host "Read existing baseline.txt, start monitoring files."
+    Write-Host "Read existing baseline.txt, start monitoring files." _ForegroundColor Yellow
  }
+  # For each file, calculate the hash, and write to baseline.txt
+  foreach ($f in $files) {
+    $hash = Calculate-File-Hash $f.FullName
+    "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+}
